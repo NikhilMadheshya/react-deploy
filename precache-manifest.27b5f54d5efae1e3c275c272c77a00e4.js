46e1b1ee06b0388e94f313aa5233fc0029d1a011
Nikhil Gupta <47312460+NikhilMadheshya@users.noreply.github.com>
2020-08-11 16:29:03 +0530

    Updates

---
+++ precache-manifest.27b5f54d5efae1e3c275c272c77a00e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "406d0ea7893aa263db824b2b65a07701",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "1f8c76f1576b5cda8235",
+    "url": "/react-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "926ac0cad321f222793f",
+    "url": "/react-deploy/static/js/2.f7035d65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.f7035d65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f8c76f1576b5cda8235",
+    "url": "/react-deploy/static/js/main.e8ad90d7.chunk.js"
+  },
+  {
+    "revision": "7c5f5b17347fb9fc8b6e",
+    "url": "/react-deploy/static/js/runtime-main.a04c58e6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
